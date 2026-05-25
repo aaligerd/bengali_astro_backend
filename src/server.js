61047e9034972e28d69728f82c4aa9ba819ffe50
aaligerd <amerpoth@gmail.com>
2026-05-25 14:51:29 +0530

    removing cors

--- src/server.js
+++ src/server.js
@@ -10,20 +10,20 @@ const PORT = process.env.PORT || 4000;
 
 // Enable CORS
 const allowedOrigins = process.env.FRONTEND_URL.split(',');
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
-
+app.use(cors("*"));
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.text({ type: "text/csv", limit: "50mb" }));
